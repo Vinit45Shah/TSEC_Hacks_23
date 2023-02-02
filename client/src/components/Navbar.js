0671a45916d2209218545655544ce87af5987785
Drishti <82228411+Drishti9421@users.noreply.github.com>
2023-02-03 04:30:19 +0530

    logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleClick=()=>{
+    localStorage.clear();
+    navigate('/')
+  }
   return (
     <div className="py-2 px-16 flex items-center bg-primary-gray justify-between border-2 border-b-primary-gray ">
       <div className="flex items-center">
@@ -10,7 +16,7 @@ const Navbar = () => {
         </a>
       </div>
       <div>
-        <button className="btn-primary ">
+        <button className="btn-primary mt-2" onClick={handleClick}>
           Logout
         </button>
       </div>
