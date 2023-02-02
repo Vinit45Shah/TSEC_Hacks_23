56328a111da39486e992ee2e0ac21d6089b0621c
Avish Jain <avishjain0@gmail.com>
2023-02-02 20:09:19 +0530

    login integration

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,53 +2,84 @@ const express = require("express");
 const users = require("../models/user.model");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 
+router.post(
+  "/registeruser",
+  [body("email").isEmail(), body("password").isLength({ min: 5 })],
+  async (req, res) => {
+    let success = false;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ success, errors: errors.array() });
+    }
+    try {
+      let user = await users.findOne({ email: req.body.email });
 
-router.post('/registeruser', 
-[
-  body("email").isEmail(),
-  body("password").isLength({ min: 5 }),
-],
-async(req, res)=>{
-  let success = false;
-  const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    res.status(400).json({success, errors : errors.array()})
-  }
-    try{
-      let user = await users.findOne({email : req.body.email});
-
-      if(user){
-        res.status(400).json({success, error:"Email ID already used"})
+      if (user) {
+        res.status(400).json({ success, error: "Email ID already used" });
       }
 
-      const salt = await bcrypt.genSalt(10)
-      const secPass = await bcrypt.hash(req.body.password, salt)
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
 
       user = await users.create({
-        name : req.body.name,
-        email : req.body.email,
-        password : secPass,
-        address : req.body.address,
-        number : req.body.number
-  
-      })
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+        address: req.body.address,
+        number: req.body.number,
+      });
       const data = {
         user: {
-          id:user.id
-        }
-      }
+          id: user.id,
+        },
+      };
 
       const authToken = jwt.sign(data, "hetvi");
 
-      success = true
-      res.json({success, authToken})
-    }catch(err){
+      success = true;
+      res.json({ success, authToken });
+    } catch (err) {
       console.log(err);
-      res.json({status : 'error', error : err})
+      res.json({ status: "error", error: err });
     }
-  })
+  }
+);
+
+router.post(
+  "/loginuser",
+  [body("email").isEmail(), body("password").isLength({ min: 5 })],
+  async (req, res) => {
+    let success = false;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ success, errors: errors.array() });
+    }
+    try {
+      let user = await users.findOne({ email: req.body.email });
+
+      if (!user) {
+        res.status(400).json({ success, error: "Email ID does not exist" });
+      } else {
+        const checkPass = bcrypt.compareSync(req.body.password, user.password);
+        if (checkPass) {
+          const data = {
+            user: {
+              id: user.id,
+            },
+          };
+          const authToken = jwt.sign(data, "hetvi");
+          success = true;
+          res.json({ success, authToken });
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      res.json({ status: "error", error: err });
+    }
+  }
+);
 
 module.exports = router;
