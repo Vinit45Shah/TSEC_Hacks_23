ed3422cc53a36a3ff470a4db1dbcb1f3530ade17
Drishti <82228411+Drishti9421@users.noreply.github.com>
2023-02-02 19:22:11 +0530

    PostMedicine Form

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = (props) => {
   return (
-    <div className='bg-primary-gray rounded-md w-1/2 p-8'>
+    <div className='rounded-md w-1/2 '>
         <div className='font-bold text-2xl py-4'>{props.heading}</div>
         <div>{props.description}</div>
     </div>
