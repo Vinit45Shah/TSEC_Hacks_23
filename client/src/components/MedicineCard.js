54fc8b87b51d7c9e4f51710668b84daa4d1e4ab2
Drishti <82228411+Drishti9421@users.noreply.github.com>
2023-02-03 03:25:22 +0530

    PostMedicine Changes

--- client/src/components/MedicineCard.js
+++ client/src/components/MedicineCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MedicineCard = (props) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg mb-8 mx-6">
-  <img src={props.img} alt="Sunset in the mountains"/>
+  <img src={props.img} alt="Medicine image"/>
   <div className="px-6 py-4">
     <div className="font-bold text-xl mb-2">{props.name}</div>
     <p className="text-gray-700 text-base">
--- client/src/pages/PostMedicine.js
+++ client/src/pages/PostMedicine.js
@@ -10,50 +10,59 @@ const PostMedicine = () => {
                 <div className='text-center font-bold text-3xl p-2 pt-6'>Donate Medicines</div>
                 <div className='flex justify-center space-x-4 '>
                     {/* <img src={bg} alt="img" className='backgroundimage relative opacity-50 rounded-lg ' /> */}
-                    <div className='rounded-lg absolute flex'>
+                    <div className='rounded-lg flex'>
                     <img src={girl} alt="image" className='-translate-y-4'/>
-                        <div className="flex flex-col min-w-full min-h-full mb-7">
+                        <div className="flex flex-col h-[100vh] mb-7 items-center justify-center">
                             <div className='py-2 px-4'>
                                 <label className="font-medium text-lg">Common Name:</label>
                                 <div className="flex flex-row flex-wrap justify-between">
-                                    <input type='text' name='commonName' className="rounded-xl border border-purple w-96 p-1" />
+                                    <input type='text' name='commonName' className="rounded-xl border border-purple w-96 p-1 text-primary-black" />
                                 </div>
                             </div>
 
                             <div className='py-2 px-4'>
                                 <label className="font-medium text-lg">Quantity:</label>
                                 <div className="flex flex-row flex-wrap justify-between">
-                                    <input type='number' name='quantity' className="rounded-xl border border-purple w-96 p-1" />
+                                    <input type='number' name='quantity' className="rounded-xl border border-purple w-96 p-1 text-primary-black"/>
                                 </div>
                             </div>
 
                             <div className='py-2 px-4'>
                                 <label className="font-medium text-lg">Expiry Date:</label>
                                 <div className="flex flex-row flex-wrap justify-between">
-                                    <input type='date' name='expiry_date' className="rounded-xl border border-purple w-96 p-1" />
+                                    <input type='date' name='expiry_date' className="rounded-xl border border-purple w-96 p-1 text-primary-black" />
                                 </div>
                             </div>
 
                             <div className='py-2 px-4'>
                                 <label className="font-medium text-lg">Scientific Name:</label>
                                 <div className="flex flex-row flex-wrap justify-between">
-                                    <input type='text' name='scientificName' className="rounded-xl border border-purple w-96 p-1" />
+                                    <input type='text' name='scientificName' className="rounded-xl border border-purple w-96 p-1 text-primary-black" />
                                 </div>
                             </div>
 
                             <div className='py-2 px-4'>
                                 <label className="font-medium text-lg">Manufacturer:</label>
                                 <div className="flex flex-row flex-wrap justify-between">
-                                    <input type='text' name='scientificName' className="rounded-xl border border-purple w-96 p-1" />
+                                    <input type='text' name='scientificName' className="rounded-xl border border-purple w-96 p-1 text-primary-black" />
                                 </div>
                             </div>
 
                             <div className='py-2 px-4'>
                                 <label className="font-medium text-lg">Picture of the medicine:</label>
                                 <div className="flex flex-row flex-wrap justify-between">
-                                    <input type='file' name='picture' className="rounded-xl border border-purple w-96 p-1" />
+                                    <input type='file' name='picture' className="rounded-xl border border-purple w-96 p-1 text-primary-black" />
                                 </div>
                             </div>
+
+                            <div className='py-2 px-4'>
+                                <label className="font-medium text-lg">Time of delivery:</label>
+                                <div className="flex flex-row flex-wrap justify-between">
+                                    <input type='string' name='picture' className="rounded-xl border border-purple w-96 p-1 text-primary-black" />
+                                </div>
+                            </div>
+
+                            <button className='btn-primary w-1/3 mx-auto'>Donate</button>
                         </div>
                     </div>
                 </div>
