6a91c0d741f0f913f2f7f49b6492b05e24e3b988
HetviSolanki49701 <hetvis.solanki@gmail.com>
2023-02-02 18:47:44 +0530

    login page and ocr

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PostMedicine from "./pages/PostMedicine";
 import UserLandingPage from "./pages/UserLandingPage";
+import Login from "./pages/Login";
+import OCR from "./components/OCR";
+
 function App() {
   return (
     <div className="App font-ourfont">
@@ -9,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/clienthome" element={<UserLandingPage/>} />
           <Route path="/postmedicine" element={<PostMedicine/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/ocr" element={<OCR/>} />
         </Routes>
         </Router>
     </div>
